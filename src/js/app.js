23f9e306afc6bf4e49f010fad790d3b253450d3f
michellejames <shellyjames90@yahoo.com>
2018-01-13 16:47:19 -0500

    First Vue Homework

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		people: [
+		{
+			name: "Shelly",
+			email: "shelljames90@yahoo.com",
+		},
+		{
+			name: "Spenser",
+			email: "hubby@soon.com",
+		},
+		{
+			name: "Sydney",
+			email: "please@sleepin.com",
+		},
+		],
+		enteredName: "Tyson",
+		enteredEmail: "tyson@cool.com",
+		// enteredEmail: function () {
+		// 	console.log("Test");
+		// }
+
+	},
+})
+
+
+
