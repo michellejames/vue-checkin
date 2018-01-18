0665db7a46bf81ca57306d720abe9d7e7c31d3d9
michellejames <shellyjames90@yahoo.com>
2018-01-17 20:04:46 -0500

    Updated components in class and added image at the end

--- src/js/app.js
+++ src/js/app.js
@@ -7,24 +7,60 @@ var app = new Vue({
 		{
 			name: "Shelly",
 			email: "shelljames90@yahoo.com",
+			time: Date.now(),
+			since: 0,
 		},
 		{
 			name: "Spenser",
 			email: "hubby@soon.com",
+			time: Date.now(),
+			since: 0,
 		},
 		{
 			name: "Sydney",
 			email: "please@sleepin.com",
+			time: Date.now(),
+			since: 0,
 		},
 		],
-		enteredName: "Tyson",
-		enteredEmail: "tyson@cool.com",
-		// enteredEmail: function () {
-		// 	console.log("Test");
-		// }
+		enteredName: "",
+		enteredEmail: "",
 
 	},
+	methods: {
+		updateSinceValues: function () {
+			this.people.forEach(person => {
+				person.since = Math.round((Date.now() - person.time)/1000/60);
+			});
+		},
+		submitPerson: function (){
+			if (!this.enteredName || !this.enteredEmail){
+				return;
+			}
+			console.log("Person submitted!");
+			this.people.unshift({
+				name: this.enteredName,
+				email: this.enteredEmail,
+				time: Date.now(),
+				since: 0,
+			})
+
+			if(this.people.length > 4) {
+				this.people.pop();
+			}
+
+			this.enteredName = "";
+			this.enteredEmail = "";
+		}
+	},
+	created: function () {
+		setInterval(this.updateSinceValues, 6000);
+	},
 })
+
+
+
+
 
 
 
